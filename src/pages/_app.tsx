aadf99443d6feb6dc22d364b0c235f4281cf7872
djaaba <ddanil82@bk.ru>
2023-07-26 11:07:47 +0300

    fixing first letter of the path on github

---
+++ src/pages/_app.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import type { AppProps } from "next/app";
+import { Roboto } from '@next/font/google';
+import { Provider } from "react-redux";
+import NextNProgress from "nextjs-progressbar";
+import "regenerator-runtime/runtime";
+import { ToastContainer } from "react-toastify";
+
+import "normalize.css";
+import "@/styles/globals.css";
+import 'react-toastify/dist/ReactToastify.css';
+
+import { rootStore } from "@/store/rootStore";
+import { Layout } from "@/components/layout/Layout";
+
+const roboto = Roboto({
+    weight: ['400', '500', '700'],
+    subsets: ['latin'],
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+
+    return (
+        <>
+            <NextNProgress
+                color="var(--red)"
+                options={{
+                    trickleSpeed: 50,
+                    showSpinner: false,
+                    speed: 300,
+                }}
+            // showAfterMs={300}
+            />
+            <Provider store={rootStore}>
+                <Layout style={roboto.style}>
+                    <Component {...pageProps} />
+                </Layout>
+            </Provider>
+            <ToastContainer />
+        </>
+    );
+}
